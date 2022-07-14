11007d26804982d5c85894281bd29ccc8cffb6ab
vinalian <vinalian23@gmail.com>
2022-07-14 20:24:31 +0300

    Start

--- Artyom_2022/Artyom_2022/views.py
+++ Artyom_2022/Artyom_2022/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
+def reverse(request):
+    user_text = request.GET['username']
+    reverse = user_text[::-1]
+    return render(request,'reverse.html',{'word':reverse})
+
 def home(request):
     return render(request, "home.html")
